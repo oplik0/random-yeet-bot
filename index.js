ed393dcb513ba3e1b70a92df0aaa5355b0920689
oplik0 <opliko.reg@protonmail.com>
2022-09-03 00:57:32 +0200

    feat: DMs - now with intent

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.DirectMessages],
+});
 
 // When the client is ready, run this code (only once)
 client.once("ready", async () => {
@@ -15,21 +17,20 @@ client.once("ready", async () => {
 		for (const channel of channels.values()) {
 			if (channel.members.size === 0) continue;
 			if (randomInt(10000) === 9999) {
-				await Promise.all(channel.members.map(member => disconnectMember(member, "Peszek²")));
+				await Promise.all(channel.members.map(member => disconnectMember(channel, member, "miał peszek²")));
 			}
-			if ((env.RANDOMLY_RUN != "true" || randomInt(0, channel.members.size + 11) > 10)) {
+			if (env.RANDOMLY_RUN != "true" || randomInt(0, 2 * channel.members.size + 100) > 100) {
 				const member = channel.members.at(randomInt(channel.members.size));
-				disconnectMember(member, "Peszek");
+				await disconnectMember(channel, member, "miał peszek");
 			}
 		}
 		exit();
 	}
 });
 
-async function disconnectMember(member, message = "Peszek") {
+async function disconnectMember(channel, member, message = "Peszek") {
 	await member.voice.disconnect(message);
-	const DMChannel = await member.createDM();
-	await DMChannel.send(message);
+	await channel.send(`${member} ${message}`);
 }
 
 // Login to Discord with your client's token
