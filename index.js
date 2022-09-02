a869e55fb598e4b155ae0561adc11866b3fb97c2
oplik0 <opliko.reg@protonmail.com>
2022-09-03 00:07:19 +0200

    feat: initial version

---
+++ index.js
@@ -0,0 +1,24 @@
+import { randomInt } from "node:crypto";
+import { env } from "node:process";
+
+import { Client, GatewayIntentBits } from "discord.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+
+// When the client is ready, run this code (only once)
+client.once("ready", async () => {
+	for (const guild of client.guilds.cache.values()) {
+		const channels = guild.channels.cache.filter(channel => channel.isVoiceBased());
+		for (const channel of channels.values()) {
+			if (!env.RANDOMLY_RUN || randomInt(0, 100) < 10) {
+				channel.members.at(randomInt(channel.memberCount)).voice.disconnect("Peszek");
+			}
+		}
+	}
+});
+
+// Login to Discord with your client's token
+client.login(env.DISCORD_TOKEN);
