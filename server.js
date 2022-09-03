532e9f5d4dc88cc29999ddfba4071a55a1ba3260
oplik0 <opliko.reg@protonmail.com>
2022-09-03 02:16:04 +0200

    feat: healthcheck

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
+app.get("/health", async (req, res) => {
+	res.json({ status: "up" });
+});
 app.get("/", async (req, res) => {
 	res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${env.DISCORD_ID}&permissions=29362176&scope=bot`);
+	res.text(`go to https://discord.com/api/oauth2/authorize?client_id=${env.DISCORD_ID}&permissions=29362176&scope=bot`);
 });
 
 app.listen(port, () => {
